a05d7c44bd2450121ef614d912ae00e6aaff6967
Kimyonghwann <kim35059@g.shingu.ac.kr>
2025-11-27 11:29:21 +0900

    [Feat] Effect Manager 제작
    노션 확인 후 스크립트 폴더 제작하여 스크립트 보기 편하게 정리

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -37,6 +37,8 @@ public class PlayerController : MonoBehaviour
 
     private bool isUIMode = false;                                      //UI 모드 설정
 
+    public GameObject effectPos;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -140,6 +142,8 @@ public class PlayerController : MonoBehaviour
             {
                 isLanding = true;
                 landingTimer = landingDuration;
+
+                EffectManager.instance.PlayEffect("착지이펙트", transform.position);
             }
         }
     }
@@ -200,6 +204,7 @@ public class PlayerController : MonoBehaviour
             if (animator != null)
             {
                 animator.SetTrigger("attackTrigger");
+                EffectManager.instance.PlayEffectWithDelay("공격이펙트", effectPos.transform.position, Quaternion.identity, 0.5f, 1.0f);
             }
         }
     }
